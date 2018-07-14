6133648bccd1bc361c2bff3c5f440d66a449216d
IMMarkXLII <aashishsadhikari@gmail.com>
2018-07-14 11:34:21 +0530

    maven war package

--- src/main/java/com/caveofprogramming/SocialNetworkApp.java
+++ src/main/java/com/caveofprogramming/SocialNetworkApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -16,8 +15,7 @@ public class SocialNetworkApp {
 	}
 	
 	@RequestMapping("/")
-	@ResponseBody
 	String hello() {
-		return "Hello World!";
+		return "home";
 	}
 }
